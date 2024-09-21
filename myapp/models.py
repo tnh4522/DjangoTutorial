378284a356d47ebd0b5a2e0c347fcb9c6071c94e
Huy TRAN <huy.tran@lundimatin.fr>
2024-09-21 13:18:37 +0700

    Relationships

--- myapp/models.py
+++ myapp/models.py
@@ -32,3 +32,18 @@ class Runner(models.Model):
 
 class Fruit(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through="Membership")
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
