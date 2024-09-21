9f6ca5f96f36d84f7b1ab82ed6b1474a6eeed94f
Huy TRAN <huy.tran@lundimatin.fr>
2024-09-21 12:48:43 +0700

    Using models

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tutorial.apps.TutorialConfig',
+    'tutorial',
+    'myapp'
 ]
 
 MIDDLEWARE = [
