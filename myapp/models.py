2b604ad5b6ff729c6c0c6325968fa4386ef3d318
Huy TRAN <huy.tran@lundimatin.fr>
2024-09-21 13:28:36 +0700

    Abstract base classes

--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Person(models.Model):
-    SHIRT_SIZES = {
-        "S": "Small",
-        "M": "Medium",
-        "L": "Large",
-    }
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+
+    def baby_boomer_status(self):
+        "Returns the person's baby-boomer status."
+        import datetime
+
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        elif self.birth_date < datetime.date(1965, 1, 1):
+            return "Baby boomer"
+        else:
+            return "Post-boomer"
+
+    @property
+    def full_name(self):
+        "Returns the person's full name."
+        return f"{self.first_name} {self.last_name}"
 
 
 class Musician(models.Model):
@@ -47,3 +60,38 @@ class Membership(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     date_joined = models.DateField()
     invite_reason = models.CharField(max_length=64)
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.TextField()
+
+    def save(self, **kwargs):
+        self.slug = slugify(self.name)
+        if (
+                update_fields := kwargs.get("update_fields")
+        ) is not None and "name" in update_fields:
+            kwargs["update_fields"] = {"slug"}.union(update_fields)
+        super().save(**kwargs)
+
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+        ordering = ["name"]
+
+
+class Unmanaged(models.Model):
+    class Meta:
+        abstract = True
+        managed = False
+
+
+class Student(CommonInfo, Unmanaged):
+    home_group = models.CharField(max_length=5)
+
+    class Meta(CommonInfo.Meta, Unmanaged.Meta):
+        pass
