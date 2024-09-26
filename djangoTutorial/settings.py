8df8f7ad0919ea98b1e9482e97c241c77f00e519
Huy TRAN <huy.tran@lundimatin.fr>
2024-09-26 09:32:45 +0700

    Quickstart

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tutorial.apps.TutorialConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
